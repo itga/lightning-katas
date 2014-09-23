196a616d3378311c0b9f78d670193205dcdb46dd
Apisitt Rattana <apisitt@rattana.org>
2014-09-23 08:20:37 +0700

    (feature) Partial testing for configuration.

--- spec/defaults.spec.js
+++ spec/defaults.spec.js
@@ -13,8 +13,18 @@ describe('configure', function () {
     //             i.e. configure({ user: 'bob', password: 'secret' }) -> { user: 'bob', password: 'secret', url: 'http://github.com' }
     //
     //             hint: you can use lodash :-)
+    it('returns a set of override values', function() {
+        expect(configure({ user: 'bob', password: 'secret' })).to.deep.equal({ user: 'bob', password: 'secret', url: 'http://github.com' });
+    });
 
     // exercise 2: try both _.merge and _.defaults
+    it('returns a set of override values and defaults', function() {
+        expect(configure({ user: 'bob', password: 'secret' })).to.deep.equal({ user: 'bob', password: 'secret', url: 'http://github.com' });
+    });
 
     // exercise 3: can you do it without lodash?
+    it('returns a set of override values and defaults', function() {
+        expect(configure({ user: 'bob', password: 'secret' })).to.deep.equal({ user: 'bob', password: 'secret', url: 'http://github.com' });
+    });
+
 });
--- src/configure.js
+++ src/configure.js
@@ -1,9 +1,25 @@
 'use strict';
+var _ = require('lodash');
 
-module.exports = function (/* overrides */) {
-    return {
+module.exports = function (overrided_user_profile) {
+//    var default_user_profile = _.defaults({
+//        user:     'user',
+//        password: 'password',
+//        url:      'http://github.com'
+//    });
+//
+//    return _.merge(default_user_profile, overrided_user_profile);
+
+    // Without lodah
+    var user_profile = {
         user:     'user',
         password: 'password',
         url:      'http://github.com'
     };
+
+    user_profile.user     = overrided_user_profile.user     || user_profile.user;
+    user_profile.password = overrided_user_profile.password || user_profile.password;
+    user_profile.url      = overrided_user_profile.url      || user_profile.url;
+
+    return user_profile;
 };
